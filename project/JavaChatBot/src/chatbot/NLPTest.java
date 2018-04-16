dcbb1d88d693ae80426348a2a6c21afc3b123ad8
ElkyLarenz <33266684+ElkyLarenz@users.noreply.github.com>
2018-04-16 00:51:07 -0500

    Add files via upload

---
+++ project/JavaChatBot/src/chatbot/NLPTest.java
@@ -0,0 +1,64 @@
+package chatbot;
+
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.*;
+
+import java.util.*;
+
+public class NLPTest {
+	public static void main(String[] args) {
+		/*
+		 * to get this to work install Stanford core nlp and reference the jar files in this project.
+		 * This is just a test class to see how it works.
+		 * Also this uses a lot of memory and I had to set mines to 3056mb.
+		 */
+		
+		/*GO TO THIS WEBSITE FOR SPEECH TAGS:
+		 * https://cs.nyu.edu/grishman/jet/guide/PennPOS.html
+		 */
+		
+		// creates a StanfordCoreNLP object, with POS tagging, lemmatization,
+		// NER, parsing, and coreference resolution
+		Properties props = new Properties();
+		props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+		// read some text in the text variable
+		//String text = "...";// ORIGINAL
+		String text = "what is the weather in Minnesota right now?";
+
+		// create an empty Annotation just with the given text
+		Annotation document = new Annotation(text);
+
+		// run all Annotators on this text
+		pipeline.annotate(document);
+
+		// these are all the sentences in this document
+		// a CoreMap is essentially a Map that uses class objects as keys and
+		// has values with custom types
+		List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+
+		for (CoreMap sentence : sentences) {
+			// traversing the words in the current sentence
+			// a CoreLabel is a CoreMap with additional token-specific methods
+			for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+				// this is the text of the token
+				String word = token.get(TextAnnotation.class);
+				// this is the POS tag of the token
+				String pos = token.get(PartOfSpeechAnnotation.class);
+				// this is the NER label of the token
+				String ne = token.get(NamedEntityTagAnnotation.class);
+				
+				//test
+				System.out.println(String.format("Print: word: [%s] pos: [%s] ne: [%s]", word, pos, ne));
+			}
+		}
+	}
+}
