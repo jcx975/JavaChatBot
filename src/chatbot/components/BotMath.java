3dcb9a789a9f3c11eca6251f218213001afaf7fb
ElkyLarenz <33266684+ElkyLarenz@users.noreply.github.com>
2018-04-23 16:54:10 -0500

    updates for weather

--- src/chatbot/components/BotMath.java
+++ src/chatbot/components/BotMath.java
@@ -6,24 +6,24 @@ import java.util.*;
 
 public class BotMath {
 	
-	private static String[] mathStrings = { "multiply", "times", "add", "plus", "subtract", "minus", "divide", "divided" };
+	private static String[] mathStrings = { "multiply", "times","*", "add", "plus","+", "subtract", "minus","-", "divide", "divided","/" };
 	public static List<String> mathOperations = new ArrayList<>( Arrays.asList( mathStrings ) );
 	
-	public static int calculate( int num1, int num2, String operation )
+	public static String calculate( int num1, int num2, String operation )
 	{
 		int result = 9999;
 		
 		operation = operation.toLowerCase();
 		
-		if ( operation.equals( "multiply" ) || operation.equals( "times" ) )
+		if ( operation.equals( "multiply" ) || operation.equals( "times" ) || operation.equals( "*" ) )
 			result = num1 * num2;
-		else if ( operation.equals( "add" ) || operation.equals( "plus" ) )
+		else if ( operation.equals( "add" ) || operation.equals( "plus" ) || operation.equals( "+" ) )
 			result = num1 + num2;
-		else if ( operation.equals( "subtract" ) || operation.equals( "minus" ) )
+		else if ( operation.equals( "subtract" ) || operation.equals( "minus" ) || operation.equals( "-" ) )
 			result = num1 - num2;
-		else if ( operation.equals( "divide" ) || operation.equals( "divided" ) )
+		else if ( operation.equals( "divide" ) || operation.equals( "divided" ) || operation.equals( "/" ) )
 			result = num1 / num2;
 		
-		return result;
+		return "The result of your query is: " + result;
 	}
 }
---
+++ src/chatbot/components/WeatherBot.java
@@ -0,0 +1,54 @@
+package chatbot.components;
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import com.google.gson.*;
+import com.google.gson.reflect.*;
+
+
+public class WeatherBot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String API_KEY = "371db33cd6e9a110a47526681330b720";
+		//String LOCATION = "Winona,us";
+		System.out.println("Please enter a location by city and country"
+				+ " to recieve the current weather." );
+		Scanner scan = new Scanner( System.in );
+		String LOCATION = scan.nextLine();
+		String urlString = "http://api.openweathermap.org/data/2.5/weather?q=" + LOCATION 
+				+ "&appid=" + API_KEY + "&units=imperial";
+		try{
+			StringBuilder result = new StringBuilder();
+			URL url = new URL(urlString);
+			URLConnection conn = url.openConnection();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = rd.readLine()) != null){
+				result.append(line);
+			}
+			rd.close();
+			System.out.println(result);
+			
+			Map<String, Object> respMap = jsonToMap(result.toString());
+			Map<String, Object> mainMap = jsonToMap(respMap.get("main").toString());
+			Map<String, Object> winMap = jsonToMap(respMap.get("wind").toString());
+			
+			
+			System.out.println("Current Temperature: " + mainMap.get("temp"));
+			System.out.println("Current Humidity: " + mainMap.get("humidity"));
+			//System.out.println("description: " + mainMap.get("description"));
+			System.out.println("Wind speeds: " + winMap.get("speed"));
+			System.out.println("Wind Angle: " + winMap.get("deg"));
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+	public static Map<String, Object> jsonToMap(String str){
+		Map<String, Object> map = new Gson().fromJson(str, new TypeToken<HashMap<String, Object>>()
+				{}.getType());
+		return map;
+	}
+
+}
